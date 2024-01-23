88931b73cdf9f7dc4aa0d6276f1f03f7f870bf83
Bruno Couto Craveiro de Melo <brunocoutomelo@hotmail.com>
2024-01-22 22:13:29 -0300

    Add project files.

---
+++ Data/Tarefa.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TarefasApi.Data
+{
+    [Table("Tarefas")]
+    public record Tarefa(int id, string Atividade, string Status);
+    
+    
+}
